d8ef6bbe22b49bcfdddb4fd08b5d01c9c5f23ee1
Leo <57991867+nKatsuhira@users.noreply.github.com>
2022-02-27 21:14:00 +0100

    Update PHPTest.php

--- PHPTest.php
+++ PHPTest.php
@@ -4,10 +4,24 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 
 $ThankYou = [
-    [
-        "bruh" => "bruh2",
-        "fuck" => "fucj3"
-    ]
+        "bruh" => [
+            "color" => "Grey",
+            "text_size" => 16,
+            "members" => [
+                "test1",
+                "test2",
+                "hurensohnvonnebenan"
+            ]
+        ],
+        "bruh2" => [
+            "color" => "Yellow",
+            "text_size" => 32,
+            "members" => [
+                "test1",
+                "test2",
+                "hurensohnvonnebenan"
+            ]
+        ]
 ];
 
 echo json_encode($ThankYou, JSON_PRETTY_PRINT);
