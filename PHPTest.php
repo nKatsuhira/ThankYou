bcca62fb579f72ff6be58b13817eb396a16a3db7
Leo <57991867+nKatsuhira@users.noreply.github.com>
2022-02-27 19:47:43 +0100

    Added PHPTest

---
+++ PHPTest.php
@@ -0,0 +1,27 @@
+<?php
+
+header("Contest-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+
+$ThankYou = [
+    "Test1" => [
+        "color" => "Yellow",
+        "text_size" => 16,
+        "members" => [
+            "test1",
+            "test2",
+            "hurensohnvonnebenan"
+        ]
+    ],
+    "Test2" => [
+        "color" => "Grey",
+        "text_size" => 16,
+        "members" => [
+            "test1",
+            "test2",
+            "hurensohnvonnebenan"
+        ]
+    ]
+]
+
+echo json_encode($ThankYou, JSON_PRETTY_PRINT);
