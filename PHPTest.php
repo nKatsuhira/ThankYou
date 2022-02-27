192de21f1ee19acc1b859ccfea5569596393bfe3
Leo <57991867+nKatsuhira@users.noreply.github.com>
2022-02-27 20:03:32 +0100

    PHPTest: This will be a long night...

--- PHPTest.php
+++ PHPTest.php
@@ -4,24 +4,18 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 
 $ThankYou = [
-    "Test1" => [
-        "color" => "Yellow",
-        "text_size" => 16,
-        "members" => [
-            "test1",
-            "test2",
-            "hurensohnvonnebenan"
+    [
+        "bruh" => [
+            "color" => "Grey",
+            "text_size" => 16,
+            "members" => [
+                "test1",
+                "test2",
+                "hurensohnvonnebenan"
+            ]
         ]
-    ],
-    "Test2" => [
-        "color" => "Grey",
-        "text_size" => 16,
-        "members" => [
-            "test1",
-            "test2",
-            "hurensohnvonnebenan"
-        ]
+
     ]
- ];
+];
 
 echo json_encode($ThankYou, JSON_PRETTY_PRINT);
